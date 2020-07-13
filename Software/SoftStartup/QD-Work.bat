07d8e7989dd1268dc40eff1add77629fecbe8dbf
dupont <dupontjoy@163.com>
2020-07-13 09:53:18 +0800

    07.13

--- Software/SoftStartup/QD-Work.bat
+++ Software/SoftStartup/QD-Work.bat
@@ -9,9 +9,5 @@ set dir2=C:\Program Files (x86)
 ::公司用軟件
 start "" "%dir%\Tencent\WeChat\WeChat.exe"
 
-::启动wifi热点
-netsh wlan set hostednetwork mode=allow ssid=My-8848 key=my888666
-netsh wlan start hostednetwork
-
 ::完成後退出
 exit
