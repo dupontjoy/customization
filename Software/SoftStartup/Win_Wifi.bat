d49295bf850960d732ce75fc3599d4366496daf0
dupont <dupontjoy@163.com>
2020-08-12 11:03:31 +0800

    08.12

--- Software/SoftStartup/Win_Wifi.bat
+++ Software/SoftStartup/Win_Wifi.bat
@@ -54,6 +54,26 @@ netsh wlan set hostednetwork mode=disallow
 Goto menu
 
 :Restart_WiFi
+::完整日期和時間
+set YY=%date:~0,4%
+set MON=%date:~5,2%
+set DD=%date:~8,2%
+set hh=%time:~0,2%
+set mm=%time:~3,2%
+set ss=%time:~6,2%
+::设置时间格式
+set date=%YY%.%MON%.%DD%-%hh%:%mm%:%ss%
+
+::小時數小于10点時的修正
+set /a hh=%time:~0,2%*1
+if %hh% LSS 10 set hh=0%hh%
+::设置时间格式
+set date=%YY%.%MON%.%DD%-%hh%:%mm%:%ss%
+
+::输出时间
+echo %date%
+
+::重启WiFi
 netsh wlan stop hostednetwork
 netsh wlan set hostednetwork mode=disallow
 netsh wlan set hostednetwork mode=allow
