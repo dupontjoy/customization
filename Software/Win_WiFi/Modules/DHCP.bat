790c24f5c028346904047f78867b6a25fc3e3f13
dupont <dupontjoy@163.com>
2020-09-16 11:47:15 +0800

    09.16

--- Software/Win_WiFi/Modules/DHCP.bat
+++ Software/Win_WiFi/Modules/DHCP.bat
@@ -4,6 +4,7 @@ MODE con: COLS=77 LINES=20
 
 :DHCP
 cls
-::设置自动获取
+::先设置为手动再自动获取
 netsh interface ip set address name="以太网" source=dhcp
+netsh interface IP add dns name="以太网" 114.114.114.114
 netsh interface ip set dns name="以太网" source=dhcp
